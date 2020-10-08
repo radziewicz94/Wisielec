fcd3a06b478b3a421238d0b2efc1cebac92dc8dc
Michał <Radziewicz94@gmail.com>
2020-10-08 16:54:45 +0200

    Wisielec project, JPA, Hibernate, Connect to Mysql workbench, Maven depedency, Junit tests

--- src/main/java/mainApplication/Game.java
+++ src/main/java/mainApplication/Game.java
@@ -6,9 +6,11 @@ import dbo.WordsDatabase;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Game {
 
+    private Scanner scanner = new Scanner(System.in);
     private DbConnection dbConnection = new DbConnection();
     WordsDatabase wordsDatabase;
 
@@ -18,24 +20,55 @@ public class Game {
         System.out.println("Wylosowane słowo to");
         int id = random.nextInt(getWord.size());
         String word = getWord.get(id);
-        System.out.println(hideWord(word));
+        hideWord(word);
     }
 
-    private StringBuilder hideWord(String word) {
+    private void hideWord(String word) {
 
         StringBuilder words = new StringBuilder(word);
         StringBuilder newWord = null;
+        String result = word;
+        char[] chars = result.toCharArray();
         for (int i = 0; i < word.length(); i++) {
             if (!Character.isWhitespace(word.charAt(i))) {
                 newWord = words.replace(i, i + 1, "*");
+
             }
         }
         System.out.println(word);
-        guessWord("test", newWord);
-        return newWord;
+        guessWord(word, newWord);
     }
 
     private void guessWord(String word, StringBuilder hiddenWord) {
+        word = word.toLowerCase();
 
+        int points = 8;
+        boolean flag = false;
+        do{
+            String letter = getLetter();
+            int value = word.indexOf(letter);
+            if(points > 0) {
+                try {
+                    hiddenWord.replace(value, value + 1, letter);
+                } catch (StringIndexOutOfBoundsException e) {
+                    System.out.println("nie zgadłeś");
+                    points--;
+                }
+            }
+            if(word.equals(hiddenWord))
+            {
+                System.out.println("Wygrałeś, Podane słowo to " + hiddenWord);
+                flag = true;
+            }
+            if(points == 0){
+                flag = true;
+            }
+            System.out.println(hiddenWord);
+        }while(!flag);
+    }
+    private String getLetter(){
+        System.out.println("Podaj litere");
+        String letter = scanner.nextLine();
+        return letter;
     }
 }
